8354fca60af8316360fc944ac62c685bf43f6523
Toregul <kazvine37@gmail.com>
2026-07-02 19:12:42 +0500

    done GetPetTest

--- src/test/java/tests/pet/GetPetTest.java
+++ src/test/java/tests/pet/GetPetTest.java
@@ -1,4 +1,27 @@
 package tests.pet;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import utils.ApiConstants;
+import utils.Endpoints;
+
 public class GetPetTest {
+
+    @Test
+    public void getFindByStatus(){
+        RestAssured
+                .given()
+                    .baseUri(ApiConstants.BASE_URL)
+                    .contentType(ApiConstants.CONTENT_TYPE)
+                    .queryParam("status", "sold")
+                .when()
+                    .get(Endpoints.PET_FIND_BY_STATUS)
+                .then()
+                    .statusCode(200)
+                    .log().all();
+
+    }
 }
---
+++ src/test/java/utils/ApiConstants.java
@@ -0,0 +1,7 @@
+package utils;
+
+public class ApiConstants {
+    public static final String BASE_URL = "https://virtserver.swaggerhub.com/sds-c05/Test_GCERT/1.0.0";
+
+    public static final String CONTENT_TYPE = "application/json";
+}
