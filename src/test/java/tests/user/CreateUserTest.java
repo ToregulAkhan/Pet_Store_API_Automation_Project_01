196c7f22782dfceef972ca3e67759069daccb880
Toregul <kazvine37@gmail.com>
2026-07-03 22:53:46 +0500

    done getUserLogin and getUserName

--- src/test/java/tests/user/CreateUserTest.java
+++ src/test/java/tests/user/CreateUserTest.java
@@ -6,6 +6,8 @@ import org.testng.annotations.Test;
 import utils.ApiConstants;
 import utils.Endpoints;
 
+import java.util.Arrays;
+
 public class CreateUserTest {
 
     @Test
@@ -17,6 +19,20 @@ public class CreateUserTest {
                 .body(new User(10, "theUser", "John", "James", "john@email.com", "12345", "12345", 1))
                 .when()
                 .post(Endpoints.USER)
+                .then().statusCode(200)
+                .log().status()
+                .log().body();
+    }
+
+    @Test
+    public void postUserCreateWithList(){
+        RestAssured
+                .given()
+                .baseUri(ApiConstants.BASE_URL)
+                .contentType(ApiConstants.CONTENT_TYPE)
+                .body(Arrays.asList(new User(10, "theUser", "John", "James", "john@email.com", "12345", "12345", 1)))
+                .when()
+                .post(Endpoints.USER_CREATE_WITH_LIST)
                 .then().statusCode(200)
                 .log().status()
                 .log().body();
--- src/test/java/tests/user/GetUserTest.java
+++ src/test/java/tests/user/GetUserTest.java
@@ -1,4 +1,43 @@
 package tests.user;
 
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import model.User;
+import org.testng.annotations.Test;
+import utils.ApiConstants;
+import utils.Endpoints;
+
 public class GetUserTest {
+    User user;
+
+    @Test
+    public void getUserName(){
+        Response response = RestAssured
+                .given()
+                .baseUri(ApiConstants.BASE_URL)
+                .contentType(ApiConstants.CONTENT_TYPE)
+                .pathParam("username", "theUser")
+                .when()
+                .get(Endpoints.USER_BY_NAME);
+
+        user = response.jsonPath().getObject("$", User.class);
+        response.then().statusCode(200)
+                .log().status()
+                .log().body();
+    }
+
+    @Test(dependsOnMethods = "getUserName")
+    public void getUserLogin(){
+        RestAssured
+                .given()
+                .baseUri(ApiConstants.BASE_URL)
+                .contentType(ApiConstants.CONTENT_TYPE)
+                .queryParam("username", user.getUsername())
+                .queryParam("password", user.getPassword())
+                .when()
+                .get(Endpoints.USER_LOGIN)
+                .then().statusCode(200)
+                .log().status()
+                .log().body();
+    }
 }
