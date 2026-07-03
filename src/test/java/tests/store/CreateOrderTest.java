4b43aec02695cb24bf2552bff0aaa4a977976da7
Toregul <kazvine37@gmail.com>
2026-07-03 22:18:27 +0500

    done postStoreOrder

--- src/test/java/tests/store/CreateOrderTest.java
+++ src/test/java/tests/store/CreateOrderTest.java
@@ -1,4 +1,29 @@
 package tests.store;
 
+import io.restassured.RestAssured;
+import model.Order;
+import org.testng.annotations.Test;
+import utils.ApiConstants;
+import utils.Endpoints;
+
+import java.util.Date;
+
 public class CreateOrderTest {
+
+    @Test
+    public void postStoreOrder(){
+        Order order = new Order(10, 198772, 7, new Date(), "approved",true);
+        RestAssured
+                .given()
+                    .baseUri(ApiConstants.BASE_URL)
+                    .contentType(ApiConstants.CONTENT_TYPE)
+                    .body(order)
+                .when()
+                    .post(Endpoints.STORE_ORDER)
+                .then()
+                    .statusCode(200)
+                    .log().status()
+                    .log().body();
+
+    }
 }
