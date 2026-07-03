cadea2e807be6c814c0b4f0f97001a6c4887293f
Toregul <kazvine37@gmail.com>
2026-07-03 22:33:06 +0500

    done postUser

--- src/test/java/model/User.java
+++ src/test/java/model/User.java
@@ -1,4 +1,57 @@
 package model;
 
 public class User {
+    public int id;
+    public String username;
+    public String firstName;
+    public String lastName;
+    public String email;
+    public String password;
+    public String phone;
+    public int userStatus;
+
+    public User(){}
+
+    public User(int id, String username, String firstName, String lastName, String email, String password, String phone, int userStatus){
+        this.id = id;
+        this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.phone = phone;
+        this.userStatus = userStatus;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public int getUserStatus() {
+        return userStatus;
+    }
 }
--- src/test/java/tests/user/CreateUserTest.java
+++ src/test/java/tests/user/CreateUserTest.java
@@ -1,4 +1,24 @@
 package tests.user;
 
+import io.restassured.RestAssured;
+import model.User;
+import org.testng.annotations.Test;
+import utils.ApiConstants;
+import utils.Endpoints;
+
 public class CreateUserTest {
+
+    @Test
+    public void postUser(){
+        RestAssured
+                .given()
+                .baseUri(ApiConstants.BASE_URL)
+                .contentType(ApiConstants.CONTENT_TYPE)
+                .body(new User(10, "theUser", "John", "James", "john@email.com", "12345", "12345", 1))
+                .when()
+                .post(Endpoints.USER)
+                .then().statusCode(200)
+                .log().status()
+                .log().body();
+    }
 }
