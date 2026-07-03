7ac9bf5f4a87db5367f092ad722eafe9d6620dba
Toregul <kazvine37@gmail.com>
2026-07-03 21:18:24 +0500

    done deletePetId

--- src/test/java/tests/pet/DeletePetTest.java
+++ src/test/java/tests/pet/DeletePetTest.java
@@ -1,4 +1,25 @@
 package tests.pet;
 
+import io.restassured.RestAssured;
+import org.testng.annotations.Test;
+import utils.ApiConstants;
+import utils.Endpoints;
+
 public class DeletePetTest {
+
+    @Test
+    public void deletePetId(){
+        RestAssured
+                .given()
+                    .baseUri(ApiConstants.BASE_URL)
+                    .contentType(ApiConstants.CONTENT_TYPE)
+                    .header("api_key", "12345")
+                    .pathParam("petId", 10)
+                .when()
+                    .delete(Endpoints.PET_BY_ID)
+                .then()
+                    .statusCode(200)
+                    .log().status()
+                    .log().body();
+    }
 }
