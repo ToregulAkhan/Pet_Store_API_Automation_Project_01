b7b3a9478ecd5b8d26965c1c027ca0dcdfa8f514
Toregul <kazvine37@gmail.com>
2026-07-03 22:22:25 +0500

    done deleteStoreOrderId

--- src/test/java/tests/store/DeleteOrderTest.java
+++ src/test/java/tests/store/DeleteOrderTest.java
@@ -1,4 +1,25 @@
 package tests.store;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.testng.annotations.Test;
+import utils.ApiConstants;
+import utils.Endpoints;
+
 public class DeleteOrderTest {
+
+    @Test
+    public void deleteStoreOrderId(){
+        RestAssured
+                .given()
+                    .baseUri(ApiConstants.BASE_URL)
+                    .contentType(ApiConstants.CONTENT_TYPE)
+                    .pathParam("orderId", 10)
+                .when()
+                    .delete(Endpoints.STORE_ORDER_BY_ID)
+                .then()
+                    .statusCode(200)
+                    .log().status()
+                    .log().body();
+    }
 }
