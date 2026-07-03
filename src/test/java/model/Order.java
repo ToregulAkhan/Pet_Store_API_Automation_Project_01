7691626b80a298cb696ad287071c91131109d427
Toregul <kazvine37@gmail.com>
2026-07-03 21:34:34 +0500

    done getStoreOrderId

--- src/test/java/model/Order.java
+++ src/test/java/model/Order.java
@@ -1,4 +1,47 @@
 package model;
 
+import java.util.Date;
+
 public class Order {
+    public int id;
+    public int petId;
+    public int quantity;
+    public Date shipDate;
+    public String status;
+    public boolean complete;
+
+    public Order(){}
+
+    public Order(int id, int petId, int quantity, Date shipDate, String status, boolean complete) {
+        this.id = id;
+        this.petId = petId;
+        this.quantity = quantity;
+        this.shipDate = shipDate;
+        this.status = status;
+        this.complete = complete;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getPetId() {
+        return petId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public Date getShipDate() {
+        return shipDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public boolean getComplete() {
+        return complete;
+    }
 }
--- src/test/java/tests/store/GetOrderTest.java
+++ src/test/java/tests/store/GetOrderTest.java
@@ -1,4 +1,31 @@
 package tests.store;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import model.Order;
+import org.testng.annotations.Test;
+import utils.ApiConstants;
+import utils.Endpoints;
+
 public class GetOrderTest {
+
+    @Test
+    public void getStoreOrderId(){
+        Response response = RestAssured
+                .given()
+                .baseUri(ApiConstants.BASE_URL)
+                .contentType(ApiConstants.CONTENT_TYPE)
+                .pathParam("orderId", 10)
+                .when()
+                .get(Endpoints.STORE_ORDER_BY_ID);
+
+        Order order = response.jsonPath().getObject("", Order.class);
+        System.out.println("id: " + order.getId());
+        System.out.println("petID: " + order.getPetId());
+        System.out.println("quantity: " + order.getQuantity());
+        System.out.println("shipDate: " + order.getShipDate());
+        System.out.println("status: " + order.getStatus());
+        System.out.println("complete: " + order.getComplete());
+    }
 }
